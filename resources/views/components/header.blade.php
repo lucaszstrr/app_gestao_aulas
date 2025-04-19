83ed31d06a166a56911fc69397ec09515625840b
Lucas <lucaaszstr@gmail.com>
2025-04-19 20:04:47 -0300

    Header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,13 +2,13 @@
 
     <div class="header_left">
     
-        <img href="{{asset("imgs/logo-financas.png")}}" alt="Reforçando">
+        <img href="{{asset("imgs/logo-reforcando.png")}}" alt="Reforçando">
 
     </div>
 
     <div class="header_right">
 
-        @include('components.button',['text' => 'Criar Conta','class' => ''])
+        @include('components.button',['text' => 'Criar Conta','class' => '', 'href' => 'createaccount'])
         @include('components.button',['text' => 'Login','class' => 'btn_login'])
 
     </div>
