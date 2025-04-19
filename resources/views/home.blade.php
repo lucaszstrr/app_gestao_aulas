8fac25e33a6848673fc2a91111426289ed5aab58
Lucas <lucaaszstr@gmail.com>
2025-04-19 20:04:21 -0300

    Home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="homepage">
+        <h1>Ambiente de gestão Reforçando®</h1>
+        <p>Este é o ambiente perfeito para o <b>gerenciamento</b> de seus alunos, eficiência e controle para professores que fazem a diferença.</p>
 
+        <img href="{{ asset('imgs/livros.png') }}">
+    </div>
 @endsection
