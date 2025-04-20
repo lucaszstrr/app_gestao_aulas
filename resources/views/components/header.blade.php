e4d6d4350f06b0e87f9da04b5496c3c68a5384ee
Lucas <lucaaszstr@gmail.com>
2025-04-19 23:51:13 -0300

    Header view

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,16 +1,21 @@
 <header>
 
     <div class="header_left">
-    
-        <img href="{{asset("imgs/logo-reforcando.png")}}" alt="Reforçando">
+        <a href="{{ route('home') }}">
+            <img src="{{asset("imgs/logo-reforcando.png")}}" alt="Reforçando">
+        </a>
+        
 
     </div>
 
     <div class="header_right">
 
-        @include('components.button',['text' => 'Criar Conta','class' => '', 'href' => 'createaccount'])
+        <button><a href="{{route('register')}}">Criar Conta</a></button>
+        <button class="btn_login"><a href="{{route('login')}}">Login</a></button>
+        <!--
+        @include('components.button',['text' => 'Criar Conta','class' => '', 'href' => "{{route('createaccount')}}"])
         @include('components.button',['text' => 'Login','class' => 'btn_login'])
-
+        -->
     </div>
     
 </header>
