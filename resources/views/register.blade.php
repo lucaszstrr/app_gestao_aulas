5583db2d6cae8ed78bd396b3b887fc5bd65f6dd6
Lucas <lucaaszstr@gmail.com>
2025-04-20 20:03:09 -0300

    Register view working clean

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,27 +4,34 @@
 <main>
     <section class="main-content">
         <div class="create-account-interface">
-            <h2 class="text-login">Seja bem-vindo(a)!</h2>
+            <h2 class="text-login">Crie sua conta agora mesmo!</h2>
 
-            <div>
-            <form method="POST" action="{{ route('register') }}">
+            <form method="POST" action="{{ route('register-account') }}">
                 @csrf
-                    <h3 class="login-input">Nome</h3>
-                    <input class="input" placeholder="Insira seu nome aqui">
+                    <div>
+                        <h3 class="login-input">Nome</h3>
+                        <input type="text" name="name" class="input" placeholder="Insira seu nome aqui" required>
 
-                    <h3 class="login-input">Email</h3>
-                    <input class="input" placeholder="Insira seu email aqui">
+                        <h3 class="login-input">Email</h3>
+                        <input type="email" name="email" class="input" placeholder="Insira seu email aqui" required>
 
-                    <h3 class="login-input">Senha</h3>
-                    <input  class="input" placeholder="Insira sua senha aqui">
+                        <h3 class="login-input">Senha</h3>
+                        <input type="password" name="password" class="input" placeholder="Insira sua senha aqui" required>
 
-                    <h3 class="login-input">Formação</h3>
-                    <input class="input" placeholder="Insira sua formação aqui">
-            
-            </div>
-        
-            <button type="submit" class="login-btn">Criar Conta</button>
+                        <h3 class="login-input">Formação</h3>
+                        <input type="text" name="area" class="input" placeholder="Insira sua formação aqui" required>
+                    </div>
+                <button linkto="{{ route('register-account') }}" type="submit" class="login-btn">Criar Conta</button>
             </form>
+
+            <!-- <form method="POST" action="{{ route('register-account') }}">
+                @csrf
+                <input type="text" name="name" class="input" placeholder="Insira seu nome aqui" required>
+                <input type="email" name="email" class="input" placeholder="Insira seu email aqui" required>
+                <input type="password" name="password" class="input" placeholder="Insira sua senha aqui" required>
+                <input type="text" name="area" class="input" placeholder="Insira sua formação aqui" required>
+                <button linkto="{{ route('register-account') }}" type="submit" class="login-btn">Criar Conta</button>
+            </form> -->
         </div>
     </section>
 </main>
