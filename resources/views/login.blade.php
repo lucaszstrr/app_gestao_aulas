e7060a7a03c6c62f8a2eba212f3a203adac1d8ae
Lucas <lucaaszstr@gmail.com>
2025-04-20 20:03:26 -0300

    Login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,11 +6,11 @@
             <h2 class="text-login">Seja bem-vindo(a)!</h2>
 
             <div>
-                <h3 class="login-input">Email</h3>
-                <input class="input" placeholder="Insira seu email aqui">
+                    <h3 class="login-input">Email</h3>
+                    <input class="input" placeholder="Insira seu email aqui">
 
-                <h3 class="login-input">Senha</h3>
-                <input  class="input" placeholder="Insira sua senha aqui">
+                    <h3 class="login-input">Senha</h3>
+                    <input  class="input" placeholder="Insira sua senha aqui">                
             </div>
         
             <button class="login-btn">Entrar</button>
