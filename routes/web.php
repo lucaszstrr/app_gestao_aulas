55197b98a5538e7eebc9913d007455a3339157df
Lucas <lucaaszstr@gmail.com>
2025-04-19 23:49:53 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,12 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 
-Route::get('/criar-conta', function(){
-    return view('createaccount');
-});
+// Route::get('/criar-conta', function(){
+//     return view('register');
+// })->name('register');
+
+Route::get('/register', [AuthController::class, 'index']);
+Route::post('/register', [AuthController::class, 'store'])->name('register');
