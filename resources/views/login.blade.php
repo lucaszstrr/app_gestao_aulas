417d743b661dbee0f6d14f368f3862f6e4f9235e
Lucas <lucaaszstr@gmail.com>
2025-04-28 08:56:29 -0300

    Login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,27 @@
 @section('content')
     <section class="main-content">
         <div class="login-interface">
-            <h2 class="text-login">Seja bem-vindo(a)!</h2>
+            <h2 class="text-login">Faça seu login aqui!</h2>
+            <form method="POST" action="{{ route('login-account') }}">
+                @csrf
+                    <div>
+                        <h3 class="login-input">Email</h3>
+                        <input class="input" name="email" type="email" placeholder="Insira seu email aqui" required>
+                        @error('credentials')
+                            <p class="field-error">Credenciais inválidas</p>
+                        @enderror
 
-            <div>
-                    <h3 class="login-input">Email</h3>
-                    <input class="input" placeholder="Insira seu email aqui">
+                        <h3 class="login-input">Senha</h3>
+                        <input  class="input" name="password" type="password" placeholder="Insira sua senha aqui" required> 
+                        @error('credentials')
+                            <p class="field-error">Credenciais inválidas</p>
+                        @enderror               
+                    </div>
 
-                    <h3 class="login-input">Senha</h3>
-                    <input  class="input" placeholder="Insira sua senha aqui">                
-            </div>
-        
-            <button class="login-btn">Entrar</button>
+                    <p class="not-account">Ainda não tem uma conta ?<a class="link-not-account" href="{{ route('register') }}"> Criar conta</a></p>
+
+                    <button linkto="{{ route('login-account') }}" type="submit" class="login-btn">Entrar</button>
+            </form>
         </div>
     </section>
 @endsection
