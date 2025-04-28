d5d518059b994c7c1e602366c95290c54a9c4003
Lucas <lucaaszstr@gmail.com>
2025-04-28 08:51:51 -0300

    Auth controller - feature logout added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,10 +16,6 @@ class AuthController extends Controller
         return view('register');
     }
 
-    public function indexLogin()
-    {
-        return view('login');
-    }
 
     public function store(Request $request)
     {
@@ -30,11 +26,29 @@ class AuthController extends Controller
             "area" => "required | string | max:100"
         ]);
 
+        if(strlen($validateUser["name"]) <= 2){
+            return back()->withInput()->withErrors([
+                "name-lenght" => "Nome inválido"
+            ]);
+        }
+
         $validateEmail = User::where('email', $validateUser['email'])->first();
 
         if($validateEmail){
             return back()->withInput()->withErrors([
                 "email" => "Este email já foi utilizado!"
+            ]);
+        }
+
+        if(strlen($validateUser['password'] < 7)){
+            return back()->withInput()->withErrors([
+                'password' => 'A senha deve ter no mínimo 7 caracteres'
+            ]);
+        }
+
+        if(strlen($validateUser["area"]) <= 2){
+            return back()->withInput()->withErrors([
+                "area-lenght" => "Area inválido"
             ]);
         }
 
@@ -50,23 +64,33 @@ class AuthController extends Controller
         return redirect()->route('home');
     }
 
+
+    public function indexLogin()
+    {
+        return view('login');
+    }
+
     public function login(Request $request)
     {
         $validateLogin = $request->validate([
             "email" => "required | email | max:100",
-            "password" => "required | string | min:7"
+            "password" => "required | string"
         ]);
 
-        $user = User::find($validateLogin["email"], "email");
-
-        if($user){
-            return "esse usuario ja existe";
+        if(Auth::attempt($validateLogin)){
+            return redirect()->route('menu-professor');
+        }else{
+            return back()->withInput()->withErrors([
+                "credentials" => "As credenciais não coincidem"
+            ]);
         }
 
-        if(Hash::check($validateLogin['password'], $user['password'])){
-            Auth::login($user);
+    }
+    
+    public function logout()
+    {
+        Auth::logout();
 
-            return redirect()->route('home');
-        }
+        return redirect()->route('home');
     }
 }
