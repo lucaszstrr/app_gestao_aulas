513e418cb907811b73a90ef2ba1fbdafaf1ca276
Lucas <lucaaszstr@gmail.com>
2025-05-09 16:48:24 -0300

    Student Migration

--- database/migrations/2025_04_19_164113_create_students_table.php
+++ database/migrations/2025_04_19_164113_create_students_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->integer('teacher_id');
+            $table->foreignId('teacher_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
             $table->string('responsible');
             $table->integer('age');
