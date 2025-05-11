9b495c24ae24254656640d13f4e4aa363c8e6eea
Lucas <lucaaszstr@gmail.com>
2025-05-10 21:30:10 -0300

    Feature added - when user change quantity of classes, database will register it, changing total value of classes

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Management;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ManagementController extends Controller
 {
@@ -12,13 +14,17 @@ class ManagementController extends Controller
      */
     public function index()
     {
-        //
+        $userLogged = Auth::user();
+        
+        $students = Student::where('teacher_id', $userLogged->id)->get();
+
+        return view('menu', compact('students'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Student $student)
     {
         //
     }
@@ -26,9 +32,46 @@ class ManagementController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Student $student, string $id)
     {
-        //
+        $userLogged = Auth::user();
+
+        $userId = $userLogged->id;
+
+        $findStudent = Student::findOrFail($id);
+
+        $student = Student::where('id', $id)->first();
+
+        $validateManagement = $request->validate([
+            "quantity_classes" => "required | integer | digits_between:0,12"
+        ]);
+
+        $managementExists = Management::where( "student_id", $id)->first();
+
+        if($managementExists){
+            $valueClasses = $validateManagement["quantity_classes"] * $student->class_value;
+
+            $managementExists->update([
+                "quantity_classes" => $validateManagement["quantity_classes"],
+                "total_value" => $valueClasses
+            ]);
+
+            $managementExists->save();
+
+            return redirect()->route('menu-professor')->with('success', 'Presença atualizada com sucesso!');
+        }
+
+        $valueClasses = $validateManagement["quantity_classes"] * $student->class_value;
+
+        Management::create([
+            "student_id" => $student->id,
+            "teacher_id" => $userLogged->id,
+            "quantity_classes" => $validateManagement["quantity_classes"],
+            "class_value" => $student->class_value,
+            "total_value" => $valueClasses
+        ]);
+
+        return redirect()->route('menu-professor')->with('success', 'Presença atualizada com sucesso!');
     }
 
     /**
