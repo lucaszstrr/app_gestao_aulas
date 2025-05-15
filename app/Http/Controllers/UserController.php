776641e6b6dbf5f8400e20e03f4513c32ff898cc
Lucas <lucaaszstr@gmail.com>
2025-05-15 15:06:17 -0300

    Added new functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class UserController extends Controller
 {
@@ -14,7 +15,64 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $userLogged = Auth::user();
+
+        $userName = $userLogged->name;
+
+        $time = Carbon::now('America/Sao_Paulo')->format('H:i:s');
+        
+        if($time >= '12:00:00' && $time <= '17:59:59'){
+            $greet = "Boa tarde";
+        }elseif($time >= '00:00:00' && $time <= '11:59:59'){
+            $greet = "Bom dia";
+        }elseif($time >= '18:00:00' && $time <= '23:59:59'){
+            $greet = "Boa noite";
+        }
+
+        $user = User::where("id", $userLogged->id)->first();
+
+        return view('profile', compact('greet', 'userName', 'user'));
+    }
+
+    public function addPix(Request $request)
+    {
+        $userLogged = Auth::user();
+
+        $validateKey = $request->validate([
+            "pix-key" => "required | string",
+            "pix-key-type" => "required | string"
+        ]);
+
+        if($validateKey["pix-key-type"] == 'cpf' && strlen($validateKey["pix-key"]) != 11){
+            return back()->withInput()->withErrors([
+                'cpf-key-size' => 'Tamanho da chave não é válido'
+            ]);
+        }
+
+        if($validateKey["pix-key-type"] == 'number' && strlen($validateKey["pix-key"]) != 11){
+            return back()->withInput()->withErrors([
+                'number-key-size' => 'Tamanho da chave não é válido'
+            ]);
+        }
+
+        if($validateKey["pix-key-type"] == 'cnpj' && strlen($validateKey["pix-key"]) != 14){
+            return back()->withInput()->withErrors([
+                'cnpj-key-size' => 'Tamanho da chave não é válido'
+            ]);
+        }
+
+        if($validateKey["pix-key-type"] == 'random' && strlen($validateKey["pix-key"]) != 32){
+            return back()->withInput()->withErrors([
+                'random-key-size' => 'Tamanho da chave não é válido'
+            ]);
+        }
+
+        $user = User::where("id", $userLogged->id)->first();
+        
+        $user->update($validateKey);
+        $user->save();
+
+        return redirect()->route('perfil');
     }
 
     /**
