f4d2c65b5ce955c7048983dbdd6f2bbda25de728
Lucas <lucaaszstr@gmail.com>
2025-05-16 16:39:09 -0300

    Finances controller - added function to generate pix code to owner

--- app/Http/Controllers/FinancesController.php
+++ app/Http/Controllers/FinancesController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Management;
 use App\Models\Student;
+use App\Models\User;
+use App\Services\PixService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
-
-
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class FinancesController extends Controller
 {
@@ -25,6 +26,8 @@ class FinancesController extends Controller
         $roomRental = $classes * 20;
 
         $time = Carbon::now('America/Sao_Paulo')->format('H:i:s');
+
+        $firstName = explode(' ', $userLogged->name)[0];
         
         if($time >= '12:00:00' && $time <= '17:59:59'){
             $greet = "Boa tarde";
@@ -34,6 +37,50 @@ class FinancesController extends Controller
             $greet = "Boa noite";
         }
 
-        return view('finances', compact('students', 'userLogged', 'totalClasses', 'roomRental', 'time', 'greet'));
+        return view('finances', compact('students', 'userLogged', 'totalClasses', 'roomRental', 'time', 'greet', 'firstName'));
+    }
+
+    public function rentGeneratePix()
+    {
+        $userLogged = Auth::user();
+
+        $validateUserKey = User::where("id", $userLogged->id)->first();
+
+        if($validateUserKey["pix-key"] == null){
+            return back()->withErrors([
+                "key-dont-exists" => "Chave não cadastrada"
+            ]);
+        }
+
+        $owner = User::where("owner", true)->first();
+
+        $quantityClasses = Management::where("teacher_id", $userLogged->id)->sum("quantity_classes");
+
+        $rentValue = $quantityClasses * 20;
+
+       
+
+        $loggedFirstName = explode(' ', $userLogged->name)[0];
+
+        $upperUserName = strtoupper($loggedFirstName);
+
+        $upperOwnerName = strtoupper($owner->name);
+
+        $pixService = new PixService();
+
+        $payload = $pixService->gerarPix(
+            chavePix: '+55'.$owner["pix-key"], 
+            valor: $rentValue, 
+            beneficiario: $upperOwnerName,
+            cidade: 'GUARAPUAVA',
+            descricao: 'ALUGUEL SALA PROF '.$upperUserName,
+            txid: 'ALUG' . time()
+        );
+
+        return view('payment', [
+            'codigoPix' => $payload['codigo'],
+            'qrCode' => $payload['qr_code'],
+            'aluguel' => $rentValue
+        ]);
     }
 }
