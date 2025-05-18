164fd23f343600e39900bf88da37b96501d9767c
Lucas <lucaaszstr@gmail.com>
2025-05-18 14:33:36 -0300

    Finances view

--- resources/views/finances.blade.php
+++ resources/views/finances.blade.php
@@ -6,7 +6,6 @@
         <div class="finances-content">
             <h1>Cobranças</h1>
         </div>
-
         <div class="table-responsive">
             <table class="table table-bordered table-hover" style="border-collapse: collapse;">
                 <thead class="thead-light">
@@ -27,16 +26,31 @@
 
                             $responsible = $student->responsible();
 
+                            $message = "https://wa.me/55".$student->responsible_number."?text=***Aluno:***".$student->name."%0A***Quantidade de aulas:***". $latestManagement->quantity_classes ?? 0 ."%0A***Valor:*** R$". number_format($latestManagement->total_value ?? 0, 2, ',', '.');
+
+                            $studentUpperName = strtoupper($student->name);
+
+                            $pix = $pixService->gerarPix(
+                                chavePix: "+55".$userLogged["pix-key"], 
+                                valor: "1.00", 
+                                beneficiario: $upperLoggedName,
+                                cidade: "GUARAPUAVA",
+                                descricao: 'AULAS '.$studentUpperName,
+                                txid: 'ALUG' . time()
+                            );
+
+                            $pixCode = $pix["codigo"];
+
                         @endphp
                         <tr class="{{ $paidStatus ? 'bg-success-light' : '' }}">
                             <td class="text-center align-middle">{{ $student->name }}</td>
                             <td class="text-center align-middle">{{ $student->responsible }}</td>
                             <td class="text-center align-middle">{{ $latestManagement->quantity_classes ?? 0 }}</td>
-                            <td class="text-center align-middle">R$ {{ number_format($latestManagement->total_value ?? 0, 2, ',', '.') }}</td>
+                            <td class="text-center align-middle">R${{ number_format($latestManagement->total_value ?? 0, 2, ',', '.') }}</td>
                             <td class="text-center align-middle">
-                                <a target="_blank" href="https://wa.me/55{{ $student->responsible_number }}?text=***Aluno:*** {{ $student->name }}%0A***Quantidade de aulas:*** {{ $latestManagement->quantity_classes ?? 0 }}%0A***Valor:*** R$ {{ number_format($latestManagement->total_value ?? 0, 2, ',', '.') }}">
+                                <a target="_blank" href="{{ $message }}">      
                                     <button class="green-button">
-                                        Enviar mensagem <i class="fa-solid fa-message" style="color: #113800;"></i>
+                                        Enviar mensagem <i class="fa-brands fa-whatsapp" style="color: #113800;"></i>
                                     </button>
                                 </a>
                             </td>
@@ -59,18 +73,21 @@
             <h3>{{ $greet }}, {{ $firstName }}!</h3>
             @if($totalClasses == 0)
                 <p class="finances-text">Você ainda não teve nenhuma aula!</p>
-
             @else
+                @if($totalClasses == 1)
+                    <p class="finances-text">Até o momento, você teve {{ $totalClasses }} aula!</p>
+                    <p class="finances-text">O valor das salas a ser pago é de: R${{ number_format($roomRental, 2, ',', '.') }}</p>
+                    <a href="{{ route('gerar-codigo-pix') }}">
+                        <button class="green-button">Pagar com código PIX <i class="fa-solid fa-qrcode" style="color: #113800;"></i></i></button>
+                    </a>
+                @else
                 <p class="finances-text">Até o momento, você teve {{ $totalClasses }} aulas!</p>
                 <p class="finances-text">O valor das salas a ser pago é de: R${{ number_format($roomRental, 2, ',', '.') }}</p>
                 <a href="{{ route('gerar-codigo-pix') }}">
                     <button class="green-button">Pagar com código PIX <i class="fa-solid fa-qrcode" style="color: #113800;"></i></i></button>
                 </a>
-                @error('key-dont-exists')               
-                    <p class="pix-field-error"><i class="fa-regular fa-circle-question" style="color: #ff0000;"></i> Você deve cadastrar uma chave PIX em <a href="{{ route('perfil') }}">Meu Perfil</a></p>
-                @enderror
+                @endif
             @endif
-                
         </div>
     </div>
 
