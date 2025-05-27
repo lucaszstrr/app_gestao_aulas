489a1699e718d26fc3fb5a4be590b21cb28154ad
Lucas <lucaaszstr@gmail.com>
2025-05-27 14:29:39 -0300

    User controller cleaned

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,39 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
+
+use App\Services\GreetService;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $userLogged = Auth::user();
 
         $userName = $userLogged->name;
 
-        $time = Carbon::now('America/Sao_Paulo')->format('H:i:s');
-
         $firstName = explode(' ', $userLogged->name)[0];
         
-        if($time >= '12:00:00' && $time <= '17:59:59'){
-            $greet = "Boa tarde";
-        }elseif($time >= '00:00:00' && $time <= '11:59:59'){
-            $greet = "Bom dia";
-        }elseif($time >= '18:00:00' && $time <= '23:59:59'){
-            $greet = "Boa noite";
-        }
+        $greetService = new GreetService();
+
+        $greet = $greetService->greet();
 
         $user = User::where("id", $userLogged->id)->first();
 
         return view('profile', compact('greet', 'userName', 'user', 'firstName', 'userLogged'));
     }
+
 
     public function addPix(Request $request)
     {
@@ -77,6 +69,7 @@ class UserController extends Controller
         return back()->with('success-key', 'Chave cadastrada');
     }
 
+
     public function updateUserName(Request $request)
     {
         $userLogged = Auth::user();
@@ -110,53 +103,5 @@ class UserController extends Controller
         $user->save();
 
         return back()->with('success', 'Operação realizada com sucesso!');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
-    {
-        //
     }
 }
