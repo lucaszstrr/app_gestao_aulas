11b29e24517ac2954e9a073fd0b2653bfe5ae108
Lucas <lucaaszstr@gmail.com>
2025-06-01 15:20:00 -0300

    Header updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,12 +10,8 @@
 
     <div class="header_right">
 
-        <a href="{{route('register')}}"><button>Criar Conta</button></a>
         <a href="{{route('login')}}"><button class="btn_login">Login</button></a>
-        <!--
-        @include('components.button',['text' => 'Criar Conta','class' => '', 'href' => "{{route('createaccount')}}"])
-        @include('components.button',['text' => 'Login','class' => 'btn_login'])
-        -->
+        
     </div>
     
 </header>
